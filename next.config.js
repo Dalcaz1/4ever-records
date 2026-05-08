e0e817fca5a3e08f79fbed32ce4649c2b4db702a
Dalcaz1 <dalcaz70@gmail.com>
2026-05-08 09:43:24 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      'supabase.co',
-      'supabase.in',
-      'img.discogs.com',
-    ],
-  },
-  env: {
-    NEXT_PUBLIC_STORE_NAME: '4 Ever Memories Records',
-  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
